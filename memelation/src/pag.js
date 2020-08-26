e2d31328bcb7509bca8e9a5b4ab5611636cc59a4
weverton-marcos-svg <Wevertonmarcos.842@gmail.com>
2020-08-26 18:32:37 -0300

    teste

---
+++ memelation/src/pag.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Pag(){
+    return (
+        <div>
+            <h1>Oi</h1>
+            <h2>Pag</h2>
+            <h6>DSFS</h6>
+        </div>
+
+    );
+} 
+
+export default Pag;
